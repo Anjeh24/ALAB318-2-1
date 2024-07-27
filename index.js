765ceab919873a574376161a079c0b12f11eb73e
anjeh24 <junez24@yahoo.com>
2024-07-27 11:14:46 -0400

    adding a route with parameters , in this case ':studentID'

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-//creating routes
+//creating routes with parameters using ':'
 
 app.get('/', (req, res) => {
     res.send("This girl can code mehnnnn!!!!");
+});
+app.get('/About', (req, res) => {
+   res.send ('I am called Anjeh and aspire to become a code wiz, oh I mean code witch, specifically a MERN HIGH PRIESTESS. \n Currently practicing how to write potions.');
+});
+app.get('/About/:studentID/summary', (req, res) => {
+  res.send(`Go to my about page to figure out where I learnt to code; ${req.params.studentID} `);
 })
 
 app.listen(port, () => {
